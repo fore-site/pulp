9c053c12cbfcde88d5d5fd56aae752a53275d512
Ogunsanu Oluwaseun <ogunsanu17@gmail.com>
2026-02-04 11:00:35 +0100

    add optional name field to user

--- src/models/user.py
+++ src/models/user.py
@@ -8,6 +8,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(verbose_name=_('email address'),
                                max_length=255,
                                 unique=True)
+    fullname = models.CharField(max_length=255, blank=True)
     phone_no = models.CharField(max_length=50, blank=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
