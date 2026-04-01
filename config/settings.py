ad2ec8229f0cef35bef0f3bf0984031eb2ea50c3
oluwaseun ogunsanu <ogunsanu17@gmail.com>
2026-04-01 01:28:26 +0100

    remove crispy forms

--- config/settings.py
+++ config/settings.py
@@ -11,9 +11,6 @@ database_password = os.getenv('DATABASE_PASSWORD')
 database_user = os.getenv('DATABASE_USER')
 database_name = os.getenv('DATABASE_NAME')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DEV_SECRET_KEY')
 
@@ -143,7 +140,7 @@ LOGOUT_REDIRECT_URL = 'home'
 TAILWIND_APP_NAME = 'theme'
 
 # NPM PATH FOR WINDOWS
-NPM_BIN_PATH = "C:\\Program Files\\nodejs\\npm.cmd"
+NPM_BIN_PATH = "/usr/bin/npm"
 
 AUTHENTICATION_BACKENDS = [
     'src.backends.CustomAuthBackend'
