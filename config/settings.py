fe448dd14915df612e75ae9e7117c776efd73230
oluwaseun ogunsanu <ogunsanu17@gmail.com>
2026-04-26 02:15:34 +0100

    rotate secrets

--- config/settings.py
+++ config/settings.py
@@ -147,7 +147,7 @@ AUTHENTICATION_BACKENDS = [
     'src.backends.CustomAuthBackend'
 ]
 
-CSRF_TRUSTED_ORIGINS = ['https://unboasting-avis-hypocoristically.ngrok-free.dev']
+CSRF_TRUSTED_ORIGINS = ['https://unboasting-avis-hypocoristically.ngrok-free.dev', 'https://pulp-3koe.onrender.com']
 CORS_ALLOW_HEADERS = ['Idempotency-Key']
 
 USE_DJANGO_JQUERY = True
